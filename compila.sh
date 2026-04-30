5e252027cb7162a1091d47a09e95de112befa698
Andrés Rouco López <andres.teleco.uvigo@gmail.com>
2026-04-30 20:13:25 +0200

    Soy subnormal e hice mal el fichero de compilacion, ahora está bien

--- compila.sh
+++ compila.sh
@@ -1,10 +1,10 @@
-gcc -Wall consultas.c -o consulta
-gcc -Wall admin.c -o admin
-gcc -Wall reservas.c -o reservas
-gcc -Wall pagos.c -o pagos
-gcc -Wall receptor.c -o receptor
-gcc -Wall anulaciones.c -o anulaciones
-gcc -Wall inicio.c -o inicio
+gcc -Wall consultas.c -o consulta -lpthread
+gcc -Wall admin.c -o admin -lpthread
+gcc -Wall reservas.c -o reservas -lpthread
+gcc -Wall pagos.c -o pagos -lpthread
+gcc -Wall receptor.c -o receptor -lpthread
+gcc -Wall anulaciones.c -o anulaciones -lpthread
+gcc -Wall inicio.c -o inicio -lpthread
 
 echo "PROGRAMAS COMPILADOS CORRECTAMENTE"
 
