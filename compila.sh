ed158a4f1510abbeb6518fb382c4c981482e960d
Andrés Rouco López <andres.teleco.uvigo@gmail.com>
2026-04-30 20:05:14 +0200

    ARCHIVO BASH DE COMPILACION

---
+++ compila.sh
@@ -0,0 +1,11 @@
+gcc -Wall consultas.c -o consulta
+gcc -Wall admin.c -o admin
+gcc -Wall reservas.c -o reservas
+gcc -Wall pagos.c -o pagos
+gcc -Wall receptor.c -o receptor
+gcc -Wall anulaciones.c -o anulaciones
+gcc -Wall inicio.c -o inicio
+
+echo "PROGRAMAS COMPILADOS CORRECTAMENTE"
+
+exit
