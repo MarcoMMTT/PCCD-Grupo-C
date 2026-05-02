e0c28ab0dcd3062e902fb48fe472fa568dd4b55a
Andrés Rouco López <andres.teleco.uvigo@gmail.com>
2026-05-02 22:19:00 +0200

    ADMINISTRACIONES FILE - DONE

--- administraciones.c
+++ administraciones.c
@@ -151,8 +151,242 @@ int main(int argc, char*argv[]){
 
         sem_wait(&mem->sem_prioridad_maxima_otro_nodo);
         sem_wait(&mem->sem_prioridad_maxima);
-        
 
+        if(mem->prioridad_maxima_otro_nodo > mem->prioridad_maxima){
 
+            // Prioridad máxima en otro nodo
+
+            sem_post(&mem->sem_prioridad_maxima_otro_nodo);
+            sem_post(&mem->sem_prioridad_maxima);
+
+            sem_wait(&mem->sem_turno_PAG_ADM);
+            mem->turno_PAG_ADM = 0;
+            sem_post(&mem->sem_turno_PAG_ADM);
+
+            sem_wait(&mem->sem_turno);
+            mem->turno = 0;
+            sem_post(&mem->sem_turno);
+
+            sem_wait(mem->sem_dentro);
+            mem->dentro = 0;
+            sem_post(mem->sem_dentro);
+
+            sem_wait(&mem->sem_prioridad_maxima_otro_nodo);
+            if(mem->prioridad_maxima_otro_nodo == CONSULTA){
+                sem_post(&mem->sem_prioridad_maxima_otro_nodo);
+                send_testigo_copia(mi_id, mem);
+            }else{
+                sem_post(&mem->sem_prioridad_maxima_otro_nodo);
+                send_testigo(mi_id, mem);
+            }
+
+        }else{
+
+            if(mem->prioridad_maxima_otro_nodo == mem->prioridad_maxima && mem->prioridad_maxima_otro_nodo != 0){
+
+                sem_post(&mem->sem_prioridad_maxima_otro_nodo);
+                sem_post(&mem->sem_prioridad_maxima);
+
+                sem_wait(&mem->sem_contador_procesos_mas_SC);
+                sem_wait(&mem->sem_contador_pag_adm_pendientes);
+                sem_wait(&mem->sem_prioridad_maximo_nodo);
+
+                if(mem->contador_procesos_max_SC >= EVITAR_RETENCION || (mem->contador_pag_adm_pendientes == 0 && mem->prioridad_maxima_otro_nodo != 0)){
+
+                    #ifdef __PRINT_PROCESO
+                    printf("El proceso de administracion evita exclusion mutua o no hay procesos de esta prioridad en su nodo.\n");
+                    #endif
+
+                    sem_post(&mem->sem_prioridad_maxima_otro_nodo);
+                    sem_post(&mem->sem_contador_procesos_max_SC);
+                    sem_post(&mem->sem_contador_pag_adm_pendientes);
+
+                    sem_wait(&mem->sem_turno_PAG_ADM);
+                    mem->turno_PAG_ADM = 0;
+                    sem_post(&mem->sem_turno_PAG_ADM);
+
+                    sem_wait(&mem->sem_turno);
+                    mem->turno = 0;
+                    sem_post(&mem->sem_turno);
+
+                    sem_wait(&mem->sem_dentro);
+                    mem->dentro = 0;
+                    sem_post(&mem->sem_dentro);
+
+                    sem_wait(&mem->sem_prioridad_maxima_otro_nodo);
+                    if(mem->prioridad_maxima_otro_nodo == CONSULTA){
+                        sem_post(&mem->sem_prioridad_maxima_otro_nodo);
+                        send_testigo_copia(mi_id, mem);
+                    }else{
+                        sem_post(&mem->sem_prioridad_maxima_otro_nodo);
+                        send_testigo(mi_id, mem);
+                    }
+
+                }else{
+
+                    sem_post(&mem->sem_prioridad_maxima_otro_nodo);
+                    sem_post(&mem->sem_contador_procesos_max_SC);
+                    sem_post(&mem->sem_contador_pag_adm_pendientes);
+                    sem_post(&mem->sem_pag_adm_pend);
+
+                }
+
+            }else{
+
+                sem_post(&mem->sem_prioridad_maximo_nodo);
+
+                if(mem->prioridad_maxima != 0){
+
+                    if(mem->prioridad_maxima == ANUL){
+
+                        // La prioridad más alta de mi nodo es Anulaciones
+
+                        #ifdef __PRINT_PROCESO
+                        printf("El proceso de Administracion da paso a un proceso de Anulacion.\n");
+                        #endif
+
+                        sem_post(&mem->sem_prioridad_maxima);
+                        
+                        sem_wait(&mem->sem_atendidas);
+                        sem_wait(&mem->sem_peticiones);
+                        mem->atendidas[mi_id - 1][PAG_ADM - 1] = mem->peticiones[mi_id - 1][PAG_ADM - 1];
+                        #ifdef __DEBUG
+                        printf("DEBUG --> atendidas %d, peticiones %d.\n", atendidas[mi_id - 1][PAG_ADM - 1], peticiones[mi_id - 1][PAG_ADM - 1]);
+                        #endif
+                        sem_post(&mem->sem_atendidas);
+                        sem_post(&mem->sem_peticiones);
+
+                        sem_wait(&mem->sem_contador_procesos_max_SC);
+                        mem->contador_procesos_max_SC = 0;
+                        sem_post(&mem->sem_contador_procesos_max_SC);
+
+                        sem_wait(&mem->sem_turno_PAG_ADM);
+                        mem->turno_PAG_ADM = 0;
+                        sem_post(&mem->sem_turno_PAG_ADM);
+
+                        sem_wait(&mem->sem_turno_ANUL);
+                        mem->turno_ANUL = 1;
+                        sem_post(&mem->sem_turno_ANUL);
+
+                        sem_post(&mem->sem_anul_pend);
+
+                    }else if(mem->prioridad_maxima == PAG_ADM){
+
+                        #ifdef __PRINT_PROCESO
+                        printf("El proceso de Administracion le da paso a un proceso de Administracion o Pago.\n");
+                        #endif
+
+                        sem_post(&mem->sem_prioridad_maxima);
+                        sem_post(&mem->sem_pag_adm_pend);
+
+                    }else if(mem->prioridad_maxima == RESERVA){
+
+                        // La prioridad más alta de mi nodo es Reserva
+
+                        #ifdef __PRINT_PROCESO
+                        printf("El proceso de Administracion da paso a un proceso de Reserva.\n");
+                        #endif
+
+                        sem_post(&mem->sem_prioridad_maxima);
+                        
+                        sem_wait(&mem->sem_atendidas);
+                        sem_wait(&mem->sem_peticiones);
+                        mem->atendidas[mi_id - 1][RESERVA - 1] = mem->peticiones[mi_id - 1][RESERVA - 1];
+                        #ifdef __DEBUG
+                        printf("DEBUG --> atendidas %d, peticiones %d.\n", atendidas[mi_id - 1][RESERVA - 1], peticiones[mi_id - 1][RESERVA - 1]);
+                        #endif
+                        sem_post(&mem->sem_atendidas);
+                        sem_post(&mem->sem_peticiones);
+
+                        sem_wait(&mem->sem_contador_procesos_max_SC);
+                        mem->contador_procesos_max_SC = 0;
+                        sem_post(&mem->sem_contador_procesos_max_SC);
+
+                        sem_wait(&mem->sem_turno_PAG_ADM);
+                        mem->turno_PAG_ADM = 0;
+                        sem_post(&mem->sem_turno_PAG_ADM);
+
+                        sem_wait(&mem->sem_turno_RES);
+                        mem->turno_RES = 1;
+                        sem_post(&mem->sem_turno_RES);
+
+                        sem_post(&mem->sem_res_pend);
+
+                    }else{
+
+                        #ifdef __PRINT_PROCESO
+                        printf("El proceso de Administracion da paso a un proceso de Consulta.\n");
+                        #endif
+
+                        sem_post(&mem->sem_prioridad_maxima);
+
+                        sem_wait(&mem->sem_contador_procesos_max_SC);
+                        mem->contador_procesos_max_SC = 0;
+                        sem_post(&mem->sem_contador_procesos_max_SC);
+
+                        sem_wait(&mem->sem_turno_PAG_ADM);
+                        mem->turno_PAG_ADM = 0;
+                        sem_post(&mem->sem_turno_PAG_ADM);
+
+                        sem_wait(&mem->sem_turno_CONS);
+                        mem->turno_CONS = 1;
+                        sem_post(&mem->sem_turno_CONS);
+
+                        sem_wait(&mem->sem_atendidas);
+                        sem_wait(&mem->sem_peticiones);
+                        mem->atendidas[mi_id - 1][CONSULTA - 1] = mem->peticiones[mi_id - 1][CONSULTA - 1];
+                        #ifdef __DEBUG
+                        printf("DEBUG --> atendidas %d, peticiones %d.\n", atendidas[mi_id - 1][CONSULTA - 1], peticiones[mi_id - 1][CONSULTA - 1]);
+                        #endif
+                        sem_post(&mem->sem_atendidas);
+                        sem_post(&mem->sem_peticiones);
+
+                        sem_wait(&mem->sem_nodo_master);
+                        mem->nodo_master = 1;
+                        sem_post(&mem->sem_nodo_master);
+
+                        sem_wait(&mem->sem_contador_cons_pendientes);
+                        for(int i = 0; i < mem->contador_cons_pendientes; i++){
+                            sem_post(&mem->sem_cons_pend);
+                        }
+                        sem_post(&mem->sem_contador_cons_pendientes);
+
+                    }
+
+                }else{
+
+                    sem_post(&mem->sem_prioridad_maxima);
+
+                    sem_wait(&mem->sem_atendidas);
+                    sem_wait(&mem->sem_peticiones);
+                    mem-> atendidas[mi_id - 1][PAG_ADM - 1] = me->peticiones[mi_id - 1][PAG_ADM - 1];
+                    sem_post(&mem->sem_atendidas);
+                    sem_post(&mem->sem_peticiones);
+
+                    sem_wait(&mem->sem_turno_PAG_ADM);
+                    mem->turno_ANUL = 0;
+                    sem_post(&mem->sem_turno_PAG_ADM);
+
+                    sem_wait(&mem->sem_turno);
+                    mem->turno = 0;
+                    sem_post(&mem->sem_turno);
+
+                }
+
+            }
+
+        }
+
+        gettimeofday(&timeFin, NULL);
+
+        int secondsSC = timeSC.tv_sec - timeInicio.tv_sec;
+        int microsSC = secondsSC*1000000 + timeSC.tv_usec - timeInicio.tv_usec;
+
+        int secondsSalida = timeFin.tv_sec - timeFinSC.tv_sec;
+        int microsSalida = secondsSalida*1000000 + timeFin.tv_usec - timeFinSC.tv_usec;
+
+        fprintf(ficheroSalida, "[%d,Anulaciones,%d,%d]", mi_id, microsSC, microsSalida);
+
+        return 0;
 
 }
